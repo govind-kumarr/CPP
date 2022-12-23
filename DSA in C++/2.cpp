bb784f7544a5c85eaae219dc04d6e421eef9e3a8
govind kumar <gk4051668@gmail.com>
2022-12-23 15:11:07 +0530

    added relational operators

---
+++ DSA in C++/2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /*
+  int a = 123;
+  cout << a << endl;
+
+  char character = 'v';
+  cout << character << endl;
+
+  bool boolean = true;
+  cout << boolean << endl;
+
+  float decimalNum = 1.2;
+  cout << decimalNum << endl;
+
+  int size = sizeof(a);
+  cout << "Size of a is : "<< size << endl;
+  */
+
+  /*
+   int a = 'a';
+   cout << a << endl;
+
+   char character = 101;
+   cout << character << endl;
+  */
+
+  /*
+    unsigned int a = -112;
+    cout << a << endl;
+  */
+
+  /*
+    int a = 2 / 5;
+    cout << a << endl;
+
+    float _a = 2.0 / 5;
+    cout << _a << endl;
+  */
+
+  /*Relational Operators
+  int a = 2;
+  int b = 3;
+
+  bool first = a == b;
+  cout << first << endl;
+
+  bool second = a > b;
+  cout << second << endl;
+
+  bool third = a < b;
+  cout << third << endl;
+
+  bool fourth = a >= b;
+  cout << fourth << endl;
+
+  bool five = a <= b;
+  cout << five << endl;
+  */
+}
