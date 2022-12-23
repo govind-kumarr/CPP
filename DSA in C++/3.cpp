facd7e5b57ab17083601942e30b8aa91a18e2989
govind kumar <gk4051668@gmail.com>
2022-12-23 15:57:17 +0530

    added conditionals

---
+++ DSA in C++/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /*
+  int a, b;
+  cout << "Enter value of a" << endl;
+  cin >> a;
+  cout << "Enter value of b" << endl;
+  cin >> b;
+  */
+
+  int a = 12, b = 15;
+  if (a > b)
+  {
+    cout << "a is greater" << endl;
+  }
+  else if (b > a)
+  {
+
+    cout << "b is greater" << endl;
+  }
+}
---
+++ DSA in C++/5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 3;
+
+  int i = 1;
+
+  while (i <= n)
+  {
+    int j = 1;
+    while (j <= n)
+    {
+      cout << "* ";
+      j++;
+    }
+    cout << endl;
+    i++; 
+  }
+}
