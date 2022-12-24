d72e13fba0c1feb4aa5c7f03929f22200da47fa2
govind kumar <gk4051668@gmail.com>
2022-12-24 18:43:53 +0530

    starting C++ STL

---
+++ DSA in C++/C++ STL/1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+  /*
+  int basic[3] = {1, 2, 3};
+  cout << "Starting STL" << endl;
+
+  array<int, 4> a = {4, 5, 6, 7};
+
+  int size = a.size();
+
+  for (int i = 0; i < size; i++)
+  {
+    cout << a[i] << " " << i << "th element" << endl;
+  }
+
+  cout << "Element at 2nd Index" << a.at(2) << endl;
+
+  cout << "Empty or Not"
+       << " " << a.empty() << endl;
+
+  cout << "Front element of array" << a.front() << endl;
+  cout << "Last element of array" << a.back() << endl;
+  */
+}
